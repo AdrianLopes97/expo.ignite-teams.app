c0b540f6570bc0294a9c1cfcfce95d32fac77e9a
AdrianLopes97 <adrianlopes97@rede.ulbra.br>
2025-05-11 22:03:12 -0300

    Adicionado o uso de estados e FlatList na tela "Groups"

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,14 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { HighLight } from '@components/HighLight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>(['Grupo 1', 'Grupo 2', 'Grupo 3']);
+
+
   return (
     <Container>
       <Header />
@@ -11,8 +17,15 @@ export function Groups() {
         title="Turmas"
         subtitle="Jogue com a sua turma!"
       />
-      <GroupCard 
-        title="Turma da Manhã"
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
       />
     </Container>
   );
