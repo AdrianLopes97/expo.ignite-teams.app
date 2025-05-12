5a2b52fdd0570abdee39b5b0039621c93b7d13ef
AdrianLopes97 <adrianlopes97@rede.ulbra.br>
2025-05-11 22:07:31 -0300

    Criado e atribuido o componente EmptyList

---
+++ src/components/EmptyList/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Message } from "./styles";
+
+type Props = {
+    message: string;
+}
+
+export function ListEmpty(props: Props) {
+    const { message } = props;
+    return (
+        <Container>
+            <Message>{message}</Message>
+        </Container>
+    )
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,10 +4,11 @@ import { HighLight } from '@components/HighLight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/EmptyList';
 
 export function Groups() {
 
-  const [groups, setGroups] = useState<string[]>(['Grupo 1', 'Grupo 2', 'Grupo 3']);
+  const [groups, setGroups] = useState<string[]>([]);
 
 
   return (
@@ -25,6 +26,10 @@ export function Groups() {
           <GroupCard 
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
