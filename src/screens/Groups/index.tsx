a8939d61b7f14d1601ae13d366da70594b34cf1f
AdrianLopes97 <adrianlopes97@rede.ulbra.br>
2025-05-12 16:51:19 -0300

    Criado o componente de botão para utilizar no app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/EmptyList';
+import { Button } from '@components/Button';
 
 export function Groups() {
 
@@ -31,6 +32,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   );
